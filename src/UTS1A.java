a4b9db3a4f61f738f4d39f2acba5fa462eeb7227
Kresa12 <aurakresa12@gmail.com>
2023-11-13 15:12:09 +0700

    program

--- src/UTS1A.java
+++ src/UTS1A.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
 public class UTS1A {
     public static void main(String[] args) {
-        
+
+        String kodeBarang;
+        String namaBarang;
+        String quality;
+        int harga;
+        int jumlah;
+        int totalHarga;
+
+
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.print("Masukan kode barang: ");
+        kodeBarang = input.nextLine();
+
+        System.out.print("Masukan nama barang: ");
+        namaBarang = input.nextLine();
+
+        System.out.print("Masukan quality barang: ");
+        quality = input.next();
+
+        System.out.print("Masukan harga barang: ");
+        harga = input.nextInt();
+
+        System.out.print("Masukan jumlah : ");
+        jumlah = input.nextInt();
+
+        totalHarga = jumlah * harga;
+
+        System.out.println("Kode barang: " + kodeBarang);
+        System.out.println("Nama barang: " + namaBarang);
+        System.out.println("Quality: " + quality);
+        System.out.println("Harga: " + harga);
+        System.out.println("Total harga: " + totalHarga);
+
     }
 }
